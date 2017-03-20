fa4c3f4652275dc6f3941658655bfcbe017b6e73
viaden <viaden@users.noreply.github.com>
2017-03-20 15:36:37 +0300

    Firebase integration

--- sdk/src/main/java/com/viaden/sdk/RegistrationService.java
+++ sdk/src/main/java/com/viaden/sdk/RegistrationService.java
@@ -9,9 +9,15 @@ import android.util.Log;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.android.gms.iid.InstanceID;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrationService extends IntentService {
-    private static final String TAG = "RegIntentService";
+    private static final String TAG = "RegService";
 
     public RegistrationService() {
         super(TAG);
@@ -25,11 +31,36 @@ public class RegistrationService extends IntentService {
     @Override
     protected void onHandleIntent(@Nullable final Intent intent) {
         try {
-            final InstanceID id = InstanceID.getInstance(this);
-            final String token = id.getToken("", GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
-            Log.i(TAG, "GCM Registration Token: " + token);
-        } catch (Exception e) {
+            new InstanceData(InstanceID.getInstance(this)).store(FirebaseDatabase.getInstance().getReference());
+        } catch (@NonNull final Exception e) {
             Log.d(TAG, "Failed to complete token refresh", e);
+        }
+    }
+
+    private static class InstanceData {
+        @NonNull
+        private final String id;
+        @NonNull
+        private final String gcmToken;
+        private final long creationTime;
+
+        private InstanceData(@NonNull final InstanceID instanceId) throws IOException {
+            id = instanceId.getId();
+            gcmToken = instanceId.getToken("", GoogleCloudMessaging.INSTANCE_ID_SCOPE);
+            creationTime = instanceId.getCreationTime();
+        }
+
+        @NonNull
+        private Map<String, Object> asMap() {
+            final HashMap<String, Object> result = new HashMap<>();
+            result.put("instanceId", id);
+            result.put("gcmToken", gcmToken);
+            result.put("creationTime", creationTime);
+            return result;
+        }
+
+        void store(@NonNull final DatabaseReference database) {
+            database.child("instance").child(id).updateChildren(asMap());
         }
     }
 }
