deec354d101d342f35290569f879b3b3047fd448
viaden <viaden@users.noreply.github.com>
2017-05-31 15:08:01 +0300

    Update build scripts

--- .build-scripts/travis.before_install.sh
+++ .build-scripts/travis.before_install.sh
@@ -3,29 +3,29 @@
 # Check dokumentation for more info about Infer installation on Linux
 # https://github.com/facebook/infer/blob/master/INSTALL.md#pre-compiled-versions
 
-echo -e "\e[33mInstalling Infer dependencies\e[0m"
-apt-get update
-apt-get -o Dpkg::Options::="--force-confnew" upgrade -y
-apt-get -o Dpkg::Options::="--force-confnew" install -y  \
-  autoconf \
-  automake \
-  build-essential \
-  libffi-dev \
-  libgmp-dev \
-  libmpc-dev \
-  libmpfr-dev \
-  m4 \
-  pkg-config \
-  python-software-properties \
-  unzip \
-  zlib1g-dev
+#echo -e "\e[33mInstalling Infer dependencies\e[0m"
+#apt-get update
+#apt-get -o Dpkg::Options::="--force-confnew" upgrade -y
+#apt-get -o Dpkg::Options::="--force-confnew" install -y  \
+#  autoconf \
+#  automake \
+#  build-essential \
+#  libffi-dev \
+#  libgmp-dev \
+#  libmpc-dev \
+#  libmpfr-dev \
+#  m4 \
+#  pkg-config \
+#  python-software-properties \
+#  unzip \
+#  zlib1g-dev
 
 # Opam is broken on some Ubuntu versions
 # Install Opam from official repository (http://opam.ocaml.org/doc/Install.html#Binarydistribution)
-echo -e "\e[33mInstalling Opam\e[0m"
-yes '' | add-apt-repository ppa:avsm/ppa
-apt-get update
-apt-get install -y ocaml ocaml-native-compilers camlp4-extra opam
+#echo -e "\e[33mInstalling Opam\e[0m"
+#yes '' | add-apt-repository ppa:avsm/ppa
+#apt-get update
+#apt-get install -y ocaml ocaml-native-compilers camlp4-extra opam
 
 # Checkout Infer
 echo -e "\e[33mCloning Infer from Github\e[0m"
