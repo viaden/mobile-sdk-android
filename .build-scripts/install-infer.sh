ecf3548e7975b51a43dbc7d9e614f333e1848703
viaden <viaden@users.noreply.github.com>
2017-05-31 16:56:44 +0300

    Update build scripts

--- .build-scripts/install-infer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -ex
-wget https://github.com/facebook/infer/releases/download/v0.12.0/infer-linux64-v0.12.0.tar.xz
-tar xf infer-linux64-v0.12.0.tar.xz
-cd infer-linux64-v0.12.0 && ./build-infer.sh java && make install
